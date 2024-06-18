f3a9943b3af8c66d09c271efa2aeb898470e3565
Maarten Narain <mnarain@qualogy.com>
2024-06-18 19:18:41 -0300

    b8ineary search oplossing voor nummers

--- src/sr/unasat/ad/search/binary/BinarySearchService.java
+++ src/sr/unasat/ad/search/binary/BinarySearchService.java
@@ -5,11 +5,25 @@ import sr.unasat.ad.models.Student;
 public class BinarySearchService {
 
     //schrijf een binarySearch methods voor onderstaande methoden
+    //Time Complexity: O(log n)
+    public int findByNumber(int[] sortedArray, int term) {
+        int left = 0; // min index
+        int right = sortedArray.length - 1; // max index
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
 
-    public int findByNumber(int number, int[] numbers) {
-
-        return -1; //term not found
+            if (sortedArray[mid] == term) { // search term found
+                return mid; // return sortedArray index of search term
+            } else if (term < sortedArray[mid]) { // search term is smaller than mid point value
+                right = mid - 1; // set right bound
+            } else {
+                left = mid + 1; // set left bound
+            }
+        }
+        return -1;
     }
+
+
 
     public String findByProgrammingLanguage(String programmingLanguage, String[] programmingLanguageList) {
 
