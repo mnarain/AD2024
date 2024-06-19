07da208c09978d0e3869e855e1ad333299d8bf15
Maarten Narain <mnarain@qualogy.com>
2024-06-19 19:10:28 -0300

    oplossing linear search met numbers

--- src/sr/unasat/ad/search/linear/LinearSearchService.java
+++ src/sr/unasat/ad/search/linear/LinearSearchService.java
@@ -3,10 +3,14 @@ package sr.unasat.ad.search.linear;
 import sr.unasat.ad.models.Student;
 
 public class LinearSearchService {
-
+     //Big O notation: O(n)   linear
     //schrijf een linearSearch method die een int[] data en een int searchTerm als parameter accepteert
     public int findByNumber(int number, int[] numbers) {
-
+        for (int i = 0; i < numbers.length; i++) {
+            if (number == numbers[i]) {
+                return i; //term found return index
+            }
+        }
         return -1; //term not found
     }
 
