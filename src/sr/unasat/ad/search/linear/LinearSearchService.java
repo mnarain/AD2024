11cffeb8358ce674be4ab544e1f8d5c928753623
Maarten Narain <mnarain@qualogy.com>
2024-06-19 19:15:24 -0300

    opdracht string en student retur types linear search

--- src/sr/unasat/ad/search/linear/LinearSearchService.java
+++ src/sr/unasat/ad/search/linear/LinearSearchService.java
@@ -3,7 +3,12 @@ package sr.unasat.ad.search.linear;
 import sr.unasat.ad.models.Student;
 
 public class LinearSearchService {
-     //Big O notation: O(n)   linear
+
+    public String findByProgrammingLanguage(String programmingLanguage, String[] programmingLanguageList) {
+
+        return "De text " + programmingLanguage + " komt niet voor in deze dataset"; //term not found
+    }
+
     //schrijf een linearSearch method die een int[] data en een int searchTerm als parameter accepteert
     public int findByNumber(int number, int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
@@ -15,5 +20,10 @@ public class LinearSearchService {
     }
 
 
+    //schrijf een linearSearch method die een Student[] data en een studentNummer searchTerm als parameter accepteert
+    public Student findByStudentNumber(String studentNummer, Student[] studentList) {
+
+        return null; //term not found
+    }
 
 }
