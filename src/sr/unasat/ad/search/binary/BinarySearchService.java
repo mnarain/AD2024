6493c589ddb7a981b1637324766b52b628fb920b
Maarten Narain <mnarain@qualogy.com>
2024-06-19 21:05:56 -0300

    Revert "b8ineary search oplossing voor nummers"
    This reverts commit 7c8317dd

--- src/sr/unasat/ad/search/binary/BinarySearchService.java
+++ src/sr/unasat/ad/search/binary/BinarySearchService.java
@@ -7,10 +7,33 @@ public class BinarySearchService {
     //schrijf een binarySearch methods voor onderstaande methoden
     //Time Complexity: O(log n)
     public int findByNumber(int[] sortedArray, int term) {
+        int left = 0; // min index
+        int right = sortedArray.length - 1; // max index
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
 
+            if (sortedArray[mid] == term) { // search term found
+                return mid; // return sortedArray index of search term
+            } else if (term < sortedArray[mid]) { // search term is smaller than mid point value
+                right = mid - 1; // set right bound
+            } else {
+                left = mid + 1; // set left bound
+            }
+        }
         return -1;
     }
 
 
+
+    public String findByProgrammingLanguage(String programmingLanguage, String[] programmingLanguageList) {
+
+        return "De text " + programmingLanguage + " komt niet voor in deze dataset"; //term not found
+    }
+
+
+   public Student findByStudentNumber(String studentNummer, Student[] studentList) {
+
+        return null; //term not found
+    }
 
 }
