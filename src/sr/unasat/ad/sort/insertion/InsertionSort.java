741cc8976f2ef03adbaa96f6fa18a4670e158788
Maarten Narain <mnarain@qualogy.com>
2024-06-28 18:47:07 -0300

    oplossing selection sort met numbers

--- src/sr/unasat/ad/sort/insertion/InsertionSort.java
+++ src/sr/unasat/ad/sort/insertion/InsertionSort.java
@@ -5,17 +5,34 @@ import sr.unasat.ad.models.Person;
 public class InsertionSort {
 
     /*
-    Time Complexity: O(n^2) as there are two nested loops.
-    Auxiliary Space: O(1)
-    The good thing about selection sort is it never makes
-    more than O(n) swaps and can be useful
-    when memory write is a costly operation.
-    */
+        Time Complexity: O(n^2)
+        Auxiliary Space: O(1)
+        Boundary Cases:
+        Insertion sort takes maximum time
+        to sort if elements are sorted in reverse order.
 
-    static int[] sort(int[] array) {
+        And it takes minimum time (Order of n)
+        when elements are already sorted.
+       */
+    public int[] sort(int[] array) {
+        int size = array.length;
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < size; ++firstUnsortedIndex) { //2
+            int firstUnsortedValue = array[firstUnsortedIndex];
+            int sortedIndex = firstUnsortedIndex - 1;
 
+            /* Move elements of array[0..firstUnsortedIndex-1], that are
+               greater than firstUnsortedValue, to one position ahead
+               of their current position */
+            while (sortedIndex >= 0 && array[sortedIndex] > firstUnsortedValue) {
+                //swap values of sorted part that are higher
+                array[sortedIndex + 1] = array[sortedIndex];
+                sortedIndex = sortedIndex - 1; //-1
+            }
+            array[sortedIndex + 1] = firstUnsortedValue;
+        }
         return array;
     }
+
 
     //schrijf een insertion sort gebaseert op een lijst van programming languages.
     //Tip gebruik een variant van de compareTo method. de fullname is niet case sensitive.
@@ -28,7 +45,7 @@ public class InsertionSort {
     //schrijf een insertion sort gebaseert op de persoon class en vergelijk de fullname van de personen.
     //Tip gebruik een variant van de compareTo method. de fullname is niet case sensitive.
     public Person[] sort(Person[] personList) {
-        //bubblesort logica om een lijst van personen te sorteren
+        //insertion logica om een lijst van personen te sorteren
         return personList; //lijst van gesorteerde personen returnen op basis van fullname
     }
 
