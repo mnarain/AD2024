58a4aea7531f54d9b3fa388a08918ffbd10207f2
Maarten Narain <mnarain@qualogy.com>
2024-07-10 19:53:02 -0300

    circular queque and simple linkedlist

--- src/sr/unasat/ad/linear/queue/Application.java
+++ src/sr/unasat/ad/linear/queue/Application.java
@@ -6,21 +6,21 @@ import sr.unasat.ad.models.Customer;
 public class Application {
 
     public static void main(String[] args) {
-/*        CarPriorityQueue cp = new CarPriorityQueue(5);
+        CarPriorityQueue cp = new CarPriorityQueue(5);
         cp.insert(new Car(4, "Toyota"));
-        cp.insert(new Car(2, "Honda"));
-        cp.insert(new Car(1, "BMW"));
-        cp.insert(new Car(5, "Audi"));
-        cp.insert(new Car(3, "Mercedes"));
-        cp.display();*/
-        CustomerPriorityQueue customerPriorityQueue = new CustomerPriorityQueue(5);
+        cp.insert(new Car(4, "Honda"));
+        cp.insert(new Car(3, "Ambulance met sirene"));
+        cp.insert(new Car(2, "brandweer met sirene"));
+        cp.insert(new Car(1, "Politie met Sirene"));
+        cp.display();
+/*        CustomerPriorityQueue customerPriorityQueue = new CustomerPriorityQueue(5);
         //insert 5 Customer objects
         customerPriorityQueue.insert(new Customer("John", "Levi"));
         customerPriorityQueue.insert(new Customer("Jane", "Doe"));
         customerPriorityQueue.insert(new Customer("John", "Icaza"));
         customerPriorityQueue.insert(new Customer("Jane", "Smith"));
         customerPriorityQueue.insert(new Customer("John", "Danhoe"));
-        customerPriorityQueue.display();
+        customerPriorityQueue.display();*/
     }
 
 }
